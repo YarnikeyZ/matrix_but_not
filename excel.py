c6b688f6472b985f5ce20a2c74b684b81de35067
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2022-07-14 23:02:25 +0500

    full rewrite to badder code

--- excel.py
+++ excel.py
@@ -1,58 +1,38 @@
 from subprocess import getoutput as gout
 
-rows = []
 clear = gout("clear")
 
-def matrix(s_x, s_y):
-    for n in range(1, s_x+1):
-        row = []
-        for n in range(1, s_y+1):
-            row.append('#')
-        rows.append(row)
+def get_plc_inps(mat_xy):
+    print('pos x to y')
+    plc_xy = (int(input('x = ')), int(input('y = ')))
+    sym = input('sym = ')
+    while len(sym) > 1:
+        print(f'This are more than 1 sym')
+        sym = input('sym = ')
+    while plc_xy[0] > mat_xy[0] or plc_xy[1] > mat_xy[1]:
+        print(f'To big nums')
+        plc_xy = (int(input('x = ')), int(input('y = ')))
+    return (plc_xy, sym)
 
-def addsymbol(rows, x, y, s_x, s_y, symbol):
-    print(clear)
-    if len(symbol) > 1:
-        return f'This are more than 1 symbol'
-    if x > s_x or y > s_y:
-        return f'To big num'
-    print(f'Pos = x{x}*y{y}')
-    print(f'Symbol = {symbol}')
-    columns = 'x'
-    for column_num in range(0, s_x):
-    	columns += f' {column_num+1}   '
-    columns += 'y'
-    print(columns)
-    for row, row_num in zip(rows, range(0, s_y)):
-        if row_num == y-1:
-            row[x-1] = symbol
-        print(f'{row} {row_num+1}')
- 
+
 def main():
+    print(clear)
     try:
-        global s_x, s_y
-        print(clear)
-        try:
-            print("x to y")
-            s_x = int(input('x = '))
-            s_y = int(input('y = '))
-            matrix(s_x, s_y)
-        except ValueError:
-                print("They need to be int's")
-                main()
-    except KeyboardInterrupt:
-        print("Key C pressed, exiting...")
+        print("x to y")
+        mat_xy = (int(input('x = ')), int(input('y = ')))
+        matrix = [["#"]*mat_xy[0] for y in range(mat_xy[1])]
+        while True: 
+            plc_xy, sym = get_plc_inps(mat_xy)
+            print(f'Pos = x{mat_xy[0]}*y{mat_xy[1]}\nsym = {sym}')
+            matrix[plc_xy[0]-1][plc_xy[1]-1] = sym
+            print(clear)
+            print('\n'.join(map(str, matrix)))
+    except ValueError:
+        print("They need to be int's")
         exit()
- 
+    except KeyboardInterrupt:
+        print("\nKey C pressed, exiting...")
+        exit()
+
 if __name__ == "__main__":
     main()
- 
-while True: 
-    try:
-        print('Pos x to y')
-        addsymbol(rows, int(input('x = ')), int(input('y = ')), s_x, s_y, input('symbol = '))
-    except ValueError: 
-        print("They need to be int's")
-    except KeyboardInterrupt:
-        print("Key C pressed, exiting...")
-        exit()
